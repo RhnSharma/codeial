5fe1a3613da879cdabb8c4dcfcf57b93f7ca4f32
Prateek Goel <goelprateek97@gmail.com>
2019-06-24 23:38:17 +0530

    create a sign up function

---
+++ config/mongoose.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/codeial_development');
+
+const db = mongoose.connection;
+
+
+
+db.on('error',console.error.bind(console,'error connecting to mongo db'));
+
+db.once('open',function(){
+console.log('connected to database:: MongoDB');
+
+});
+
+module.exports= db;
