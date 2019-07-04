699988a81305eb15f699a2663246e175dbb7cfc7
Prateek Goel <goelprateek97@gmail.com>
2019-07-04 15:26:23 +0530

    add scss

---
+++ config/passport-local-strategy.js
@@ -0,0 +1,76 @@
+const passport = require('passport');
+
+const LocalStrategy = require('passport-local').Strategy;
+
+const User =require('../models/user');
+
+
+//authentication using passport
+passport.use(new LocalStrategy({
+              usernameField: 'email', 
+
+},
+
+function (email,password,done){
+   //find the user and establish the identity
+     User.findOne({email:email},function(err,user){
+         if(err){
+             console.log("error in finding user --> Passport");
+             return done(err);
+         }
+
+         if(!user || user.password != password){
+             console.log('invalid username/password');
+             return done(null,false);
+         }
+           return done(null,user);
+     });
+}
+
+));
+
+// serialising the user to decide which key is to be kept in the cookies
+passport.serializeUser(function(user,done){
+        done(null,user.id);
+
+});
+
+
+// deserialising the user from the key in the cookies
+passport.deserializeUser(function(id,done){
+    User.findById(id,function(err,user){
+       if(err){
+        console.log("error in finding user --> Passport");
+        return done(err);
+       }
+
+       return done(null,user);
+    });
+});
+
+// check if user is authenticated
+
+passport.checkAuthentication = function(req,res,next){
+
+
+    // if the user is signed in thren pass on the request to the next function(controller's action)
+    if(req.isAuthenticated()){
+        return next();
+    }
+
+    //if the user is not signed in
+
+    return res.redirect('/users/sign-in');
+}
+
+passport.setAuthenticatedUser = function (req,res,next){
+    if(req.isAuthenticated()){
+        //req.user contain current signed in user from session cookie and we are sending it to locals for views
+        res.locals.user= req.user;
+
+
+    }
+    next();
+}
+
+module.exports= passport;
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -11,12 +11,22 @@ module.exports.profile = function(req,res){
 
 //render the sign up page
 module.exports.signUp=function(req,res){
+
+   if(req.isAuthenticated()){
+      return  res.redirect('/users/profile');
+   }
+   
     return res.render('user_sign_up',{
         title:'Codeial | Sign up'
     })
 }
 // render the sign in page
 module.exports.signIn= function(req,res){
+
+    if(req.isAuthenticated()){
+        return  res.redirect('/users/profile');
+     }
+
     return res.render('user_sign_in',{
         title:'Codeial | Sign in'
     })
@@ -53,7 +63,15 @@ module.exports.create= function(req,res){
 
 //sign in and create a session
 module.exports.createSession= function(req,res){
-    // todo  later
+    
    
+     return res.redirect('/');
+}
+
+
+module.exports.destroySession = function(req,res){
+
+    req.logout();
+    return res.redirect('/');
 
 }
